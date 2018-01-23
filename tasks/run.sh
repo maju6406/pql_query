a6202e7a39239f910f89c3674016cf834c6e0618
maju6406 <beersy@gmail.com>
2018-01-22 16:38:56 -0800

    Initial commit

---
+++ tasks/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if puppet config print server | grep -v -q `hostname`; then
+  echo "This task can only be run on the master node."; 
+  exit 1
+fi
+echo "query:" ${query}
+unixtime_string="$(date +%s)"
+puppet query '$PT_query' &>/tmp/query_$unixtime_string
+echo "Query results can be found here: /tmp/query_" ${unixtime_string}
