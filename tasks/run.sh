3f8f261787ab7469be894ff74c47a74ce597cb68
maju6406 <beersy@gmail.com>
2018-01-22 17:01:59 -0800

    Adjusting output

--- tasks/run.sh
+++ tasks/run.sh
@@ -3,10 +3,11 @@ if puppet config print server | grep -v -q `hostname`; then
   echo "This task can only be run on the master node."; 
   exit 1
 fi
-echo "query:" ${query}
+echo "query:" ${PT_query}
 echo '---'
 unixtime_string="$(date +%s)"
-/opt/puppetlabs/bin/puppet-query "$PT_query" &>/tmp/query_$unixtime_string
+/opt/puppetlabs/bin/puppet-query '$PT_query' &>/tmp/query_$unixtime_string
 cat /tmp/query_$unixtime_string
+echo
 echo '---'
 echo "Query results can be found here: /tmp/query_"${unixtime_string}
