a6e99c449a77e563305da1a03077a4e92f328453
maju6406 <beersy@gmail.com>
2018-01-22 17:35:05 -0800

    Changed output to YAML

--- tasks/run.sh
+++ tasks/run.sh
@@ -4,11 +4,14 @@ if puppet config print server | grep -v -q `hostname`; then
   exit 1
 fi
 echo "query:" ${PT_query}
-echo '---'
+echo 'Results (YAML):'
 unixtime_string="$(date +%s)"
-filename="pqlquery_$unixtime_string.json"
-/opt/puppetlabs/bin/puppet-query "$PT_query" &>/tmp/$filename
-cat /tmp/$filename
+json_filename="pqlquery_$unixtime_string.json"
+yaml_filename="pqlquery_$unixtime_string.yaml"
+/opt/puppetlabs/bin/puppet-query "$PT_query" &>/tmp/$json_filename
+ruby -e "require 'json'; puts (JSON.pretty_generate JSON.parse(STDIN.read))" < /tmp/$json_filename > /tmp/$yaml_filename
+/opt/puppetlabs/puppet/bin/ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(STDIN.read))' < /tmp/$json_filename > /tmp/$yaml_filename
+cat /tmp/$yaml_filename
 echo
-echo '---'
-echo "Query results can be found here: /tmp/"${filename}
+echo "Query results (YAML) can be found here: /tmp/"${yaml_filename}
+echo "Query results (JSON) can be found here: /tmp/"${json_filename}
