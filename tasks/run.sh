de542ee081658c8e617b4590d9fa34e16e6a169b
Abir Majumdar <beersy@gmail.com>
2019-03-08 11:46:12 -0500

    Adding some guard rails

--- tasks/run.sh
+++ tasks/run.sh
@@ -4,11 +4,15 @@
 #  exit 1
 #fi
 echo "query:" ${PT_query}
+if [[ $PT_query == *"puppet query"* ]];
+  echo "You don't need 'puppet query' in front of the query. Just add the query itself. Example: resources[file]{type = 'File'}"
+  exit 1
+fi
 echo 'Results (YAML):'
 unixtime_string="$(date +%s)"
 json_filename="pqlquery_$unixtime_string.json"
 yaml_filename="pqlquery_$unixtime_string.yaml"
-/opt/puppetlabs/bin/puppet-query "$PT_query" &>/tmp/$json_filename
+/opt/puppetlabs/bin/puppet-query "$PT_query" &>/tmp/$json_filename 2>&1
 /opt/puppetlabs/puppet/bin/ruby -e "require 'json'; puts (JSON.pretty_generate JSON.parse(STDIN.read))" < /tmp/$json_filename > /tmp/$yaml_filename
 /opt/puppetlabs/puppet/bin/ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(STDIN.read))' < /tmp/$json_filename > /tmp/$yaml_filename
 cat /tmp/$yaml_filename
